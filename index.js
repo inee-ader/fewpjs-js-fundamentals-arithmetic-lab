8ae0c3214ee180326ec4e82626c7de9b527861be
xoliio <xoliio@gmail.com>
2020-11-25 16:54:34 -0800

    blah

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+const random = Math.max(Math.random()*10)
+
+let mod1 = 15
+let mod2 = 11
+const mod = mod1 % mod2
+
+const max = Math.max(2, 3, 17, 20, 11, 19)
+
+
